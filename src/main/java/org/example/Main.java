bde2fa1e56ebfc7d1e50fb07454608b3fc787be9
Nethsara <nethsarasiyum2@gmail.com>
2023-04-16 22:08:42 +0530

    recreated hibernate util with hibernate.cfg.xml

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,27 @@
 package org.example;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Environment;
-import org.hibernate.mapping.MetadataSource;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        //Hibernate util class created with static constructor
-        try ( Session session = HibernateUtil.getSessionFactory().openSession()){
-            Object singleResult = session.createNativeQuery("SELECT NOW()").getSingleResult();
-
-        }
-
+        printMySQlVersion();
+        printMySQlDateAndTime();
     }
+
+    private static void printMySQlVersion() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Object result =
+                    session.createNativeQuery("SELECT VERSION()").getSingleResult();
+            System.out.println(result);
+        }
+    }
+
+    private static void printMySQlDateAndTime() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Object result =
+                    session.createNativeQuery("SELECT NOW()").getSingleResult();
+            System.out.println(result);
+        }
+    }
+
 }
