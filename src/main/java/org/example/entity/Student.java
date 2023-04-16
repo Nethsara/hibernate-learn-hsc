fdf2d601be55128d814436b3c21b5b1c473a8ed8
Nethsara <nethsarasiyum2@gmail.com>
2023-04-16 22:22:53 +0530

    create an entity

---
+++ src/main/java/org/example/entity/Student.java
@@ -0,0 +1,54 @@
+package org.example.entity;
+
+//@Entity(name = "student_table")
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+//==================================
+//@Entity
+//@Table(name = "student_table")
+@Entity
+public class Student {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long studentID;
+    @Column(name = "name", length = 45, nullable = false)
+    private String studentName;
+    private double marks;
+
+    public Student(long studentID, String studentName, double marks) {
+        this.studentID = studentID;
+        this.studentName = studentName;
+        this.marks = marks;
+    }
+
+    public Student() {
+    }
+
+    public long getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(long studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    public void setMarks(double marks) {
+        this.marks = marks;
+    }
+}
